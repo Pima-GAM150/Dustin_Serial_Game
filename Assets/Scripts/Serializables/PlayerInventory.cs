fffe72a8029a5d5e8402c1e34a441369e30abc08
Dustman <dreed12@mail.pima.edu>
2018-10-24 13:01:14 -0700

    Made player ui and play Menu
    also created many many scripts for the game

---
+++ Assets/Scripts/Serializables/PlayerInventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization;
+using System;
+
+[Serializable]
+public class PlayerInventory
+{
+
+	
+}
